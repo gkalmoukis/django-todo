3ee1deebdc06de02ba4a855e76214d8e939d5f85
gkalmoukis <giorgoskalmoukis@gmail.com>
2021-11-16 01:34:23 +0200

    Add dummy tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class TaskTestCase(TestCase):
+    def test_home_endpoint(self):
+        client = APIClient()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_index_endpoint(self):
+        client = APIClient()
+        response = client.get('/tasks/')
+        self.assertEqual(response.status_code, 200)
+    
